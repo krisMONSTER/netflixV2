9c3ca4e9948d60d2a2c3bf83cdc8df139e4b3e11
krisMONSTER <62694355+krismonster@users.noreply.github.com>
2020-11-02 23:48:19 +0100

    Menu animation

--- src/main/webapp/JavaScript/menuTrigger.js
+++ src/main/webapp/JavaScript/menuTrigger.js
@@ -7,16 +7,18 @@ function setVisibilityToElements(elements, visibility){
 }
 
 function menuTrigger() {
+    const menu = $("#accountMenu");
     if(opened === true){
-        setVisibilityToElements(document.getElementsByClassName("menuOption"),"hidden");
-        $("#accountMenu").animate({height: '0'},"fast",function(){
+        setVisibilityToElements(document.getElementsByClassName("menuElement"),"hidden");
+        menu.animate({height: '0'},"fast",function(){
             document.getElementById("accountMenu").style.visibility = "hidden";
         });
     }
     else {
         document.getElementById("accountMenu").style.visibility = "visible";
-        $("#accountMenu").animate({height: "20px"},"fast",function(){
-            setVisibilityToElements(document.getElementsByClassName("menuOption"),"visible");
+        menu.animate({height: menu.get(0).scrollHeight},"fast",function(){
+            $(this).height('auto');
+            setVisibilityToElements(document.getElementsByClassName("menuElement"),"visible");
         });
     }
     opened = opened === false;
